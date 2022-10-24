eb65c3e1df5cc512e8f49c256a490a379550225d
Qunliang Xing <ryanxingql@gmail.com>
2022-10-24 10:42:18 +0800

    Support symlink images

--- powerqe/datasets/qe_folder_dataset.py
+++ powerqe/datasets/qe_folder_dataset.py
@@ -1,10 +1,60 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 # Modified by RyanXingQL, 2022
+import os
 import os.path as osp
+from pathlib import Path
 
 from mmedit.datasets import SRFolderDataset
 
 from .registry import DATASETS
+
+IMG_EXTENSIONS = ('.jpg', '.JPG', '.jpeg', '.JPEG', '.png', '.PNG', '.ppm',
+                  '.PPM', '.bmp', '.BMP', '.tif', '.TIF', '.tiff', '.TIFF')
+
+
+def scandir(dir_path, suffix=None, recursive=False, case_sensitive=True):
+    """Scan a directory to find the interested files.
+
+    Args:
+        dir_path (str | :obj:`Path`): Path of the directory.
+        suffix (str | tuple(str), optional): File suffix that we are
+            interested in. Default: None.
+        recursive (bool, optional): If set to True, recursively scan the
+            directory. Default: False.
+        case_sensitive (bool, optional) : If set to False, ignore the case of
+            suffix. Default: True.
+
+    Returns:
+        A generator for all the interested files with relative paths.
+    """
+    if isinstance(dir_path, (str, Path)):
+        dir_path = str(dir_path)
+    else:
+        raise TypeError('"dir_path" must be a string or Path object')
+
+    if (suffix is not None) and not isinstance(suffix, (str, tuple)):
+        raise TypeError('"suffix" must be a string or tuple of strings')
+
+    if suffix is not None and not case_sensitive:
+        suffix = suffix.lower() if isinstance(suffix, str) else tuple(
+            item.lower() for item in suffix)
+
+    root = dir_path
+
+    def _scandir(dir_path, suffix, recursive, case_sensitive):
+        for entry in os.scandir(dir_path):
+            if not entry.name.startswith('.') and (
+                    entry.is_file() or osp.isfile(osp.realpath(entry.path))):
+                rel_path = osp.relpath(entry.path, root)
+                _rel_path = rel_path if case_sensitive else rel_path.lower()
+                if suffix is None or _rel_path.endswith(suffix):
+                    yield rel_path
+            elif recursive and os.path.isdir(entry.path):
+                # scan recursively if entry.path is a directory
+                yield from _scandir(entry.path, suffix, recursive,
+                                    case_sensitive)
+
+    return _scandir(dir_path, suffix, recursive, case_sensitive)
 
 
 @DATASETS.register_module()
@@ -41,6 +91,27 @@ class QEFolderDataset(SRFolderDataset):
         self.gt_folder = str(gt_folder)
         self.filename_tmpl = filename_tmpl
         self.data_infos = self.load_annotations()
+
+    def scan_folder(self, path):
+        """Obtain image path list (including sub-folders) from a given folder.
+
+        Args:
+            path (str | :obj:`Path`): Folder path.
+
+        Returns:
+            list[str]: image list obtained form given folder.
+        """
+
+        if isinstance(path, (str, Path)):
+            path = str(path)
+        else:
+            raise TypeError("'path' must be a str or a Path object, "
+                            f'but received {type(path)}.')
+
+        images = list(scandir(path, suffix=IMG_EXTENSIONS, recursive=True))
+        images = [osp.join(path, v) for v in images]
+        assert images, f'{path} has no valid image file.'
+        return images
 
     def load_annotations(self):
         """Load annotations for SR dataset.
