df4695cbadb83c276906d8f9faf37086ca27e689
Qunliang Xing <ryanxingql@gmail.com>
2022-12-12 15:17:56 +0800

    Bug fixed

--- configs/arcnn/arcnn_c64c32c16k9k7k1k5_div2k_lmdb_ps128_bs32_1000k_g1.py
+++ configs/arcnn/arcnn_c64c32c16k9k7k1k5_div2k_lmdb_ps128_bs32_1000k_g1.py
@@ -2,13 +2,18 @@ _base_ = './arcnn_c64c32c16k9k7k1k5_div2k_ps128_bs32_1000k_g1.py'
 
 exp_name = 'arcnn_c64c32c16k9k7k1k5_div2k_lmdb_ps128_bs32_1000k_g1'
 
+train_lq_folder = './data/div2k/train/lq_patches.lmdb'
+train_gt_folder = './data/div2k/train/gt_patches.lmdb'
+valid_lq_folder = './data/div2k/valid/lq.lmdb'
+valid_gt_folder = './data/div2k/valid/gt.lmdb'
+
 # dataset settings
 train_pipeline = [
     dict(
         type='LoadImageFromFile',
         #     io_backend='disk',
         io_backend='lmdb',
-        db_path='./data/div2k/train/lq_patches.lmdb',
+        db_path=train_lq_folder,
         key='lq',
         flag='color',
         channel_order='rgb'),
@@ -16,7 +21,7 @@ train_pipeline = [
         type='LoadImageFromFile',
         #     io_backend='disk',
         io_backend='lmdb',
-        db_path='./data/div2k/train/gt_patches.lmdb',
+        db_path=train_gt_folder,
         key='gt',
         flag='color',
         channel_order='rgb'),
@@ -36,7 +41,7 @@ test_pipeline = [
         type='LoadImageFromFile',
         #  io_backend='disk',
         io_backend='lmdb',
-        db_path='./data/div2k/train/lq.lmdb',
+        db_path=valid_lq_folder,
         key='lq',
         flag='color',
         channel_order='rgb'),
@@ -44,7 +49,7 @@ test_pipeline = [
         type='LoadImageFromFile',
         #  io_backend='disk',
         io_backend='lmdb',
-        db_path='./data/div2k/train/gt.lmdb',
+        db_path=valid_gt_folder,
         key='gt',
         flag='color',
         channel_order='rgb'),
@@ -53,25 +58,24 @@ test_pipeline = [
     dict(type='ImageToTensor', keys=['lq', 'gt'])
 ]
 
-data = dict(
-    train=dict(dataset=dict(_delete_=True,
-                            type='SRLmdbDataset',
-                            lq_folder='./data/div2k/train/lq_patches.lmdb',
-                            gt_folder='./data/div2k/train/gt_patches.lmdb',
-                            pipeline=train_pipeline,
-                            scale=1)),
-    val=dict(_delete_=True,
-             type='SRLmdbDataset',
-             lq_folder='./data/div2k/valid/lq.lmdb',
-             gt_folder='./data/div2k/valid/gt.lmdb',
-             pipeline=test_pipeline,
-             scale=1),
-    test=dict(_delete_=True,
-              type='SRLmdbDataset',
-              lq_folder='./data/div2k/valid/lq.lmdb',
-              gt_folder='./data/div2k/valid/gt.lmdb',
-              pipeline=test_pipeline,
-              scale=1))
+data = dict(train=dict(dataset=dict(_delete_=True,
+                                    type='SRLmdbDataset',
+                                    lq_folder=train_lq_folder,
+                                    gt_folder=train_gt_folder,
+                                    pipeline=train_pipeline,
+                                    scale=1)),
+            val=dict(_delete_=True,
+                     type='SRLmdbDataset',
+                     lq_folder=valid_lq_folder,
+                     gt_folder=valid_gt_folder,
+                     pipeline=test_pipeline,
+                     scale=1),
+            test=dict(_delete_=True,
+                      type='SRLmdbDataset',
+                      lq_folder=valid_lq_folder,
+                      gt_folder=valid_gt_folder,
+                      pipeline=test_pipeline,
+                      scale=1))
 
 # runtime settings
 work_dir = f'./work_dirs/{exp_name}'
